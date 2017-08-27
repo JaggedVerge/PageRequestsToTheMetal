0f2cc362114e1b529ee3b556b56c9eb967946606
Janis <janis@lesinskis.com>
2017-08-26 19:17:34 -0700

    Install python pip dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,8 @@
 apt-get update
 
 # install pip
-apt-get install -y python-pip
+apt-get install -y python3-pip
+apt-get install -y python3-venv
 
 # update to use the most recent pip
-pip install --upgrade pip
+pip3 install --upgrade pip
