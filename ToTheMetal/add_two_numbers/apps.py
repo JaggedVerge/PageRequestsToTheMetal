06811ccbcd507622dc7f6d62324a2210324c7532
Janis <janis@jaggedverge.com>
2017-08-28 03:36:18 +0000

    Create add two numbers django app

---
+++ ToTheMetal/add_two_numbers/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AddTwoNumbersConfig(AppConfig):
+    name = 'add_two_numbers'
---
+++ ToTheMetal/add_two_numbers/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def add_two_numbers_page(request):
+    """Add two numbers"""
+    return HttpResponse("Not implemented yet")
